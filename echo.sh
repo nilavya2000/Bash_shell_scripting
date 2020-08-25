37371b1210efc2e167514aebf9bf07107ab0c22f
nilavya2000 <dnilavya@gmail.com>
2020-08-25 21:06:11 +0530

    basic_of_echo

---
+++ echo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# writing the default echo
+echo "--------------------------------------"
+echo "Hello World"
+echo "Welcom to the world of coding"
+
+
+# avoid going to the next line 
+echo "----------------------------------"
+echo -n "Hello World... "
+echo  "Welcome to the world of coding ..!!"
+
+# while writing a paragraph
+echo "------------------------------------"
+echo "Hey world \n Welcome to the world of \t computing."
+
+# avoid writing \n ,\t ... as a comment and use it as a func 
+echo "-------------------------------------"
+echo -e "Hey World \nWelcome to the world of \t computing.!"
