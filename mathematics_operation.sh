03a69b2809ed6f071ab76c32cee3f78d75e51933
nilavya2000 <dnilavya@gmail.com>
2020-08-25 21:25:03 +0530

    math_operation

---
+++ mathematics_operation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# add 
+
+((sum=25+25))
+
+echo "addition 25+25 = "$sum
+
+((mul=25*25))
+
+echo "Multiplication 25*25 = "$mul
+
+((div=46/2))
+
+echo "Division 46/4 = "$div
+
+((subs=58-29))
+
+echo "Substraction 58-29 = "$subs
+
+
